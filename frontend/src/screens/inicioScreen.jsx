08adf873deaa9409c274626fc8150aea26445ae8
Liliana Ramos <a01644969@tec.mx>
2026-05-16 14:53:07 -0600

    mejora

---
+++ frontend/src/screens/inicioScreen.jsx
@@ -0,0 +1,11 @@
+//frontend\src\screens\inicioScreen.jsx
+import React from "react";
+
+const InicioScreen = () => {
+  return (
+    <div>
+      <h1>LUMI</h1>
+    </div>
+  );
+};
+export default InicioScreen;
